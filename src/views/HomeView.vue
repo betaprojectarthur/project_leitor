2f95b7d0d11516429abd13a06dcdcd13086966dc
unknown <Suporte@TJMT-1136.DSS.LOCAL>
2024-06-14 16:13:15 -0300

    14/06/2024

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,34 @@
-<script setup>
-</script>
-
 <template>
-  <div class="bg-gray-900 flex pt-12 w-full text-white font-playfair-display">
+  <div class="bg-gray-800 flex pt-12 w-full text-white h-[700px]">
+    <div class="w-2/4 p-4 flex-col justify-between">
+      <div class="flex gap-2">
+        <div class="tag">Criatividade</div>
+        <div class="tag">Habilidades Técnicas</div>
+        <div class="tag">Usabilidade</div>
+      </div>
+      <div class="my-4">
+        <h1 class="text-6xl font-playfair-display">O universo das interfaces excepcionais</h1>
+      </div>
+      <div class="w-3/4 my-4">
+        <p class="text-sm font-playfair-display">Como web designer, eu crio e desenvolvo interfaces visuais atraentes e
+          funcionais para sites. Meu trabalho combina criatividade e habilidades técnicas para proporcionar uma
+          experiência de usuário intuitiva e envolvente. Cada projeto é uma oportunidade de transformar ideias em
+          realidade digital, garantindo que o design não apenas impressione visualmente, mas também seja fácil de usar e
+          acessível para todos os usuários.</p>
+      </div>
+      <div><button class="button" type="button"><a href="">Sobre mim</a></button></div>
+    </div>
+    <div>456</div>
+  </div>
+  <div class="h-[200px]" >
+
+  </div>
+
+
+
+
+  <!-- 
+  <div class="bg-gray-800 flex pt-12 w-full text-white font-playfair-display">
     <div class="p-2 w-2/4 text-center flex justify-center items-start">
       <div class="w-2/4">
         <p class="text-start text-sm">Ola, Eu sou</p>
@@ -24,8 +50,19 @@
         <i class="fa-brands fa-linkedin"></i>
         <i class="fa-brands fa-github"></i>
         <i class="fa-brands fa-instagram"></i>
-        <i class="fa-solid fa-at"></i>
+        <i class="fa-regular fa-envelope"></i>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
+
+<script setup></script>
+<style scoped>
+.tag {
+  @apply p-2 border rounded-full text-gray-300 text-xs border-gray-600
+}
+
+.button {
+  @apply p-2 rounded-lg bg-indigo-600 text-indigo-100 text-sm border border-indigo-600 hover:bg-indigo-100 hover:text-indigo-600
+}
+</style>
